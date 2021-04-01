877639ac78e5766c68c4bc260eeff17dd43d5091
jecabm <32487257+jecabm@users.noreply.github.com>
2021-04-01 19:53:32 +1000

    case study

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("burger").style.display = "none";
+  document.getElementById("social").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("social").style.height = "0%";
+  document.getElementById("burger").style.display = "block";
+}
